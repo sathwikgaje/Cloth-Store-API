baa301af8a92ee5e6d943b7853aa2a00f228f0e9
Sathwik <sathwik.gaje111@gmail.com>
2022-09-19 23:45:31 +0530

    First Commit

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const multer = require('multer')
 const path = require('path')
 const winston = require('winston')
 const config = require('config')
+const mongoose = require('mongoose')
 const home = require('./router/home')
 const user = require('./router/user')
 const errorMiddleware = require('./middlewares/error')
 const FileFilter = require('./startup/filefilter')
 const FileStorage = require('./startup/filestorage')
 const logger = require('./startup/logging')
-require('./startup/db')()
 
 const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
@@ -24,7 +24,12 @@ app.use('/user',user)
 app.use(errorMiddleware)
 require('./startup/prod')(app)
 
-
+mongoose.connect(config.get('db'),{useUnifiedTopology: true})
+        .then(()=>{
+            logger.log({level:'info',message:`Connected to the Database ${config.get('db')}...`})
+            console.log(`Connected to the Database...`)
+        })
+        .catch((err)=>logger.log({level: 'info',message:err}))
 
 const port = process.env.PORT || 3000
 const server = app.listen(port,()=>{
